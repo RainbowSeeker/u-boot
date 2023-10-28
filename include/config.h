d583fbe8ad85f8f0e7ddbf961d10a1fb9e9c2be6
yangyu <1911466766@qq.com>
2023-10-28 19:41:20 +0800

    fork from u-boot

---
+++ include/config.h
@@ -0,0 +1,6 @@
+/* Automatically generated - do not edit */
+#define CFG_BOARDDIR board/freescale/mpc837xerdb
+#include <configs/MPC837XERDB.h>
+#include <asm/config.h>
+#include <linux/kconfig.h>
+#include <config_fallbacks.h>
